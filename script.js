33e2ebf2e0bcd73747ebf1260b63bc07e7692975
olvsivkov <1000listov@mail.ru>
2022-08-14 17:44:29 +0300

    feat: added some beauty to the user's screen

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
-const buttonsNum = document.querySelector(".buttons")
-const userScreen = document.querySelector(".userScren")
-const btnExpression = document.querySelector(".btn-expression")
-const btnReset = document.querySelector(".btn-reset")
-let expression = ""
+const buttonsNum = document.querySelector(".buttons");
+const userScreen = document.querySelector(".userScreen");
+const btnExpression = document.querySelector(".btn-expression");
+const btnReset = document.querySelector(".btn-reset");
+let expression = "";
 
-buttonsNum.addEventListener("click", calcFuction)
+buttonsNum.addEventListener("click", calcFucntion);
 
-function calcFuction(event) {
+function calcFucntion(event) {
     let target = event.target;
     if (target.classList.contains('math')) {
         expression += target.innerHTML
         userScreen.value = expression
+        localStorage.setItem("result", userScreen.value);
     }
-}
+};
 let result
 
-btnExpression.addEventListener("click", calcExpression)
+btnExpression.addEventListener("click", calcExpression);
 
 function calcExpression() {
     if (eval(expression) === undefined) window.location.reload();
     else userScreen.value = (eval(expression)).toFixed(2);
     localStorage.setItem("result", userScreen.value);
-}
+};
 
 window.addEventListener("load", () => {
-    let localStorageItem = localStorage.getItem("result");
-    if(localStorageItem !== null) userScreen.value =  localStorageItem;
-})
+    let localStorageResult = localStorage.getItem("result");
+    if(localStorageResult !== null) userScreen.value = localStorageResult;
+});
 
-btnReset.addEventListener("click", calcReset)
+btnReset.addEventListener("click", calcReset);
 
 function calcReset() {
     userScreen.value = "";
     expression = "";
     localStorage.removeItem("result");
-}
+};
+
